f0f41092809c69dd692b137c98ffd11ad99e1ddd
gitprojectsmanage <gitprojectsmanage@gmail.com>
2022-08-09 18:54:48 +0530

    chore: practice code

--- pages/templates.tsx
+++ pages/templates.tsx
@@ -1,10 +1,19 @@
+import AuthButton from '../components/buttons/auth/AuthButton';
+import CatCard from '../components/cards/cat/CatCard';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import { NextPageWithLayout } from './page';
 
 const Templates: NextPageWithLayout = () => {
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
-      Hi
+      <AuthButton />
+      <CatCard
+        tag={'123'}
+        title={'234'}
+        body={'456'}
+        author={'789'}
+        time={Date.now().toString()}
+      />
     </section>
   );
 };
