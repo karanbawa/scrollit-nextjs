f479654b2d5f2a65ae2cb7b53f93da58454f17ca
gitprojectsmanage <gitprojectsmanage@gmail.com>
2022-08-09 18:17:00 +0530

    fix: removed unused variabes

--- pages/domain.tsx
+++ pages/domain.tsx
@@ -1,26 +1,9 @@
-import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import { NextPageWithLayout } from './page';
 
 const Domain: NextPageWithLayout = () => {
-  const { locale } = useRouter();
-
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
-      {/* <Image
-        src="/Google.png"
-        alt="Google Logo"
-        width={272}
-        height={92}
-        priority
-      />
-      <Search />
-      <p>
-        Google offered in:{' '}
-        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
-          <a className="underline text-blue-600"> Français</a>
-        </Link>
-      </p> */}
       Hi
     </section>
   );
--- pages/templates.tsx
+++ pages/templates.tsx
@@ -1,10 +1,7 @@
-import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import { NextPageWithLayout } from './page';
 
 const Templates: NextPageWithLayout = () => {
-  const { locale } = useRouter();
-
   return (
     <section className="flex flex-col items-center gap-y-5 mt-12 sm:mt-36">
       Hi
